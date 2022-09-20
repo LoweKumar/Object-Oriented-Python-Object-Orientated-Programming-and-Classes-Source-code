b77d620d9309b8377b387fd5f7e3b617fca0aa0e
LoweKumar <lowekr00@gmail.com>
2022-09-20 14:09:11 +0530

    Lecture337

--- bike_oop.py
+++ bike_oop.py
@@ -1,15 +1,25 @@
 class bike(object):
 
     def __init__(self, brand, color):
-        self.brand = 'Bajaj'
-        self.color = 'Blue'
+        self.brand = brand
+        self.color = color
         self.on = False
 
 
 pulsar = bike("Bajaj", "Blue")
-print(pulsar.brand)
-print(pulsar.color)
+print(f"Pulsar brand : {pulsar.brand}")
+print(f"Pulsar color : {pulsar.color}")
 print()
 pulsar.color = "Red"
-print(pulsar.color)
-print(pulsar.brand)
+print(f"Pulsar color : {pulsar.color}")
+print(f"Pulsar brand : {pulsar.brand}")
+print()
+classic = bike("RoyalEnfield", "StealthBlack")
+print(f"Classic brand : {classic.brand}")
+print(f"Classic color : {classic.color}")
+print()
+pulsar.price = 150 # creating new object for pulsar
+print(f"Pulsar price : {pulsar.price}")
+classic.price = 180 # creating new object for RE classic
+print(f"Royal Enfield Classic price : {classic.price}")
+
