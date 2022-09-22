56224a6d535b60ae8743af7a24fd6d672f159b3c
LoweKumar <lowekr00@gmail.com>
2022-09-22 16:39:22 +0530

    Lecture356 - Calling Super Method

--- enemy.py
+++ enemy.py
@@ -18,4 +18,12 @@ class Enemy:
 
 
 class Troll(Enemy):
-    pass
+
+    def __init__(self, name):
+        # Enemy.__init__(self, name=name, hit_points=23, lives=5) #used in till python 2.9
+        # super(Troll, self).__init__(name=name, hit_points=23, lives=5) # super is used in python 3 and above
+        super().__init__(name=name, hit_points=23, lives=5) # alternate best way
+
+    def grunt(self):
+        print("Me {0.name}. {0.name} stomp you".format(self))
+
--- main.py
+++ main.py
@@ -34,21 +34,25 @@ tim = Player("Tim")
 
 from enemy import Enemy, Troll
 
-random_monster = Enemy("Monster1", 50, 5)
-print(random_monster)
-random_monster.take_damage(20)
-print(random_monster)
-random_monster.take_damage(30)
-print(random_monster)
-print("Since no Hit points left so lives will now get reduced by 1")
-random_monster.take_damage(20)
-print(random_monster)  # since no Hit points left so lives will now get reduced by 1
+# random_monster = Enemy("Monster1", 50, 5)
+# print(random_monster)
+# random_monster.take_damage(20)
+# print(random_monster)
+# random_monster.take_damage(30)
+# print(random_monster)
+# print("Since no Hit points left so lives will now get reduced by 1")
+# random_monster.take_damage(20)
+# print(random_monster)  # since no Hit points left so lives will now get reduced by 1
 
-ugly_troll = Troll()
+ugly_troll = Troll("Pug")
 print("Ugly troll - {}".format(ugly_troll))
 
-another_troll = Troll("Ug", 18, 1)
+another_troll = Troll("Ug")
 print("Another troll -{}".format(another_troll))
 
-brother = Troll("Urg", 23)
+brother = Troll("Urg")
 print(brother)
+
+ugly_troll.grunt()
+another_troll.grunt()
+brother.grunt()
