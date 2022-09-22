3f1fbe57350a900a7ec2ff6db45ac5ec2e8e8ff8
LoweKumar <lowekr00@gmail.com>
2022-09-22 13:55:55 +0530

    Lecture352 - Data Attributes and Properties

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-# import player
 from player import Player
 
 tim = Player("Tim")
-print(tim)
-# print(tim.score)
-# print(tim)
-# print(tim.level)
-print()
-tim.lives-=1
+
+print(tim.name)
+print(tim.lives)
+tim.lives -= 1
 print(tim)
 
-print()
-tim.lives-=1
+tim.lives -= 1
 print(tim)
 
-print()
-tim.lives-=1
+tim.lives -= 1
 print(tim)
 
-print()
-tim.lives-=1
+tim.lives -= 1
 print(tim)
 
+tim._lives = 9
+print(tim)
 
+tim.level = 2
+print(tim)
+
+tim.level += 5
+print(tim)
+
+tim.level = 3
+print(tim)
