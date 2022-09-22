4be0ebbd4bc22d8eba54a1349a8ef48d65e1a11d
LoweKumar <lowekr00@gmail.com>
2022-09-22 13:38:56 +0530

    Lecture351 - Getters and Setters

---
+++ main.py
@@ -0,0 +1,25 @@
+# import player
+from player import Player
+
+tim = Player("Tim")
+print(tim)
+# print(tim.score)
+# print(tim)
+# print(tim.level)
+print()
+tim.lives-=1
+print(tim)
+
+print()
+tim.lives-=1
+print(tim)
+
+print()
+tim.lives-=1
+print(tim)
+
+print()
+tim.lives-=1
+print(tim)
+
+
